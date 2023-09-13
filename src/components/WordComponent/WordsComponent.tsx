def1932ce7ada22ece994e7cc4a24904f0e906ae
Александр Казаков <29281112@s.mfua.ru>
2023-09-13 14:51:54 +0300

    README.md

--- src/components/WordComponent/WordsComponent.tsx
+++ src/components/WordComponent/WordsComponent.tsx
@@ -1,13 +1,13 @@
 import {useAppSelector} from "@/store/hooks.ts";
-import {charState, fetchRandomQuote} from "@/store/reducers/quoteSlice.tsx";
+import {charState, fetchRandomQuote, quoteType} from "@/store/reducers/quoteSlice.tsx";
 import {useEffect} from "react";
 import {useDispatch} from "react-redux";
 import {Action, ThunkDispatch} from "@reduxjs/toolkit";
 import "./WordComponent.css";
 
 const WordsComponent = () => {
-    const quote = useAppSelector(state => state.quote.value)
-    const cursor = useAppSelector(state => state.quote.cursor)
+    const quote: quoteType = useAppSelector(state => state.quote.value)
+    const cursor: number = useAppSelector(state => state.quote.cursor)
     const dispatch = useDispatch<ThunkDispatch<unknown,unknown,Action>>()
 
     useEffect(()=>{
@@ -17,7 +17,7 @@ const WordsComponent = () => {
     return(
         <div className={"flex flex-wrap"}>
             {quote?.map(({char, state}, i) => (
-                <span key={char+"_"+i} className={`${i === cursor ? 'cursor' : ''} font-firaCode ${state === charState.notTyped ? 'text-gray-500': state === charState.typedCorrect ? '' : 'text-red-500'}`}
+                <span key={char+"_"+i} className={`${i === cursor ? 'cursor' : ''} font-firaCode ${state === charState.notTyped ? 'text-gray-500': state === charState.typedCorrect ? '' :char !== " " ? 'text-red-500':"bg-red-500/50"}`}
                 >
                     {char !== " " ? char : '\u00A0'}
                 </span>
